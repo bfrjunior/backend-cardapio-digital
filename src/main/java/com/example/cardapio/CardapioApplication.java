e202722e8fbd81c0473cde0251f41f06bf03c62d
Fernando Rocha <beneditofernandor@gmail.com>
2023-10-15 10:02:07 -0300

    config

--- src/main/java/com/example/cardapio/CardapioApplication.java
+++ src/main/java/com/example/cardapio/CardapioApplication.java
@@ -3,7 +3,9 @@ package com.example.cardapio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+
 public class CardapioApplication {
 
 	public static void main(String[] args) {
